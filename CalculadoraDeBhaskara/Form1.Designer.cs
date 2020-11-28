feac1f32d823609fa2215bef83488a6e6adafa3f
Daniel Souza <67985375+DanielSouza2005@users.noreply.github.com>
2020-11-27 22:39:08 -0300

    Add files via upload

---
+++ CalculadoraDeBhaskara/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace CalculadoraDeBhaskara
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblX1 = new System.Windows.Forms.Label();
+            this.lblX2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rbReais = new System.Windows.Forms.RadioButton();
+            this.rbComplexos = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(35, 178);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnCalcular.Size = new System.Drawing.Size(246, 125);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "B";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(314, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "C";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(36, 121);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 4;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(181, 121);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 5;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(317, 121);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(116, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(221, 48);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Entre com os valores de \'A\', \'B\' e \'C\'\r\nDa forma \r\nAX^2 + BX + C = 0\r\n";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblX1
+            // 
+            this.lblX1.AutoSize = true;
+            this.lblX1.Location = new System.Drawing.Point(305, 243);
+            this.lblX1.Name = "lblX1";
+            this.lblX1.Size = new System.Drawing.Size(23, 13);
+            this.lblX1.TabIndex = 8;
+            this.lblX1.Text = "X1:";
+            // 
+            // lblX2
+            // 
+            this.lblX2.AutoSize = true;
+            this.lblX2.Location = new System.Drawing.Point(305, 290);
+            this.lblX2.Name = "lblX2";
+            this.lblX2.Size = new System.Drawing.Size(23, 13);
+            this.lblX2.TabIndex = 9;
+            this.lblX2.Text = "X2:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(383, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ajuda";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // rbReais
+            // 
+            this.rbReais.AutoSize = true;
+            this.rbReais.Location = new System.Drawing.Point(308, 178);
+            this.rbReais.Name = "rbReais";
+            this.rbReais.Size = new System.Drawing.Size(36, 17);
+            this.rbReais.TabIndex = 11;
+            this.rbReais.TabStop = true;
+            this.rbReais.Text = "IR";
+            this.rbReais.UseVisualStyleBackColor = true;
+            // 
+            // rbComplexos
+            // 
+            this.rbComplexos.AutoSize = true;
+            this.rbComplexos.Location = new System.Drawing.Point(308, 201);
+            this.rbComplexos.Name = "rbComplexos";
+            this.rbComplexos.Size = new System.Drawing.Size(35, 17);
+            this.rbComplexos.TabIndex = 12;
+            this.rbComplexos.TabStop = true;
+            this.rbComplexos.Text = "IC";
+            this.rbComplexos.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 333);
+            this.Controls.Add(this.rbComplexos);
+            this.Controls.Add(this.rbReais);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblX2);
+            this.Controls.Add(this.lblX1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblX1;
+        private System.Windows.Forms.Label lblX2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbReais;
+        private System.Windows.Forms.RadioButton rbComplexos;
+    }
+}
+
---
+++ CalculadoraDeBhaskara/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraDeBhaskara
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rbReais.Checked = true;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Calculadora de Bhaskara"
+                + Environment.NewLine
+                + "Versão 0.2"
+                + Environment.NewLine
+                + "Desenvolvedor: Daniel");
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            Double a, b, c, x1, x2, delta;
+
+            try
+            {
+                a = Convert.ToDouble(txtA.Text);
+                b = Convert.ToDouble(txtB.Text);
+                c = Convert.ToDouble(txtC.Text);
+
+                delta = b * b - 4 * a * c;
+                if (delta < 0 & rbReais.Checked == true)
+                {
+                    lblX1.Text = "X1: Delta é negativo";
+                    lblX2.Text = "X2: Delta é negativo";
+                }
+                else if(delta < 0 & rbComplexos.Checked == true)
+                {
+                    x1 = (-b + Math.Sqrt(-delta)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(-delta)) / (2 * a);
+                    lblX1.Text = "X1: " + x1.ToString() + "i";
+                    lblX2.Text = "X2: " + x2.ToString() + "i";
+                }
+                else 
+                {
+                    x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                    x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                    lblX1.Text = "X1: " + x1.ToString();
+                    lblX2.Text = "X2: " + x2.ToString();
+                }
+            }
+
+
+            catch
+            {
+                MessageBox.Show("Digite apenas números !!"
+                    + Environment.NewLine
+                    + "Probido campos em brancos e letras");
+                
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+/*
+V 0.1
+Calcula X1 e X2
+
+V 0.1.1
+Corrigiu o delta negativo
+
+V 0.1.2
+Corrigiu a mensagem do delta que aparecia junto à resposta anterior
+
+V 0.1.3
+Corrigiu os espaços em branco e letras digitadas como valores
+
+V 0.2
+Adicionada a possibilidade de escolher o conjunto numérico (reais e complexos)
+para cálculo das raízes
+*/
